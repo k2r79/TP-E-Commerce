b60f2cb7dd185127ce587ac01a8b475358a439e8
fredoprog <fredoprog@gmail.com>
2014-04-14 16:52:01 +0200

    modification du schema et ajout d'un fichier .sql

---
+++ procedures_fait_par_frederic.sql
@@ -0,0 +1,30 @@
+
+/* Produits bientôt hors de stock (seuil) */
+
+CREATE OR REPLACE PROCEDURE produitsBientotHorsDeStock (seuil IN number) 
+  IS
+ 	 quantiteStock number(4,0);
+  BEGIN
+ 
+ 	SELECT stock INTO quantiteStock
+ 	FROM produit;
+ 
+ 	IF quantiteStock <= seuil THEN
+ 	  dbms_output.put('Ce produits est bientôt hors de stock');	  
+	END IF;
+
+END produitsBientotHorsDeStock; 
+
+
+/*Changer le statut d'une commande (commande, statut)*/
+
+CREATE PROCEDURE changerLeStatutDuneCommmande(commande commande%ROWTYPE, statut statut%ROWTYPE)
+	IS
+	
+	
+	BEGIN 
+	
+	
+	
+	
+	END changerLeStatutDuneCommmande;
