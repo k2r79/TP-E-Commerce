e374bfc88b0a20336521ddeed8a0b572b7a98358
fredoprog <fredoprog@gmail.com>
2014-04-15 11:33:20 +0200

    commit insert client

---
+++ Insert/insert_client.sql
@@ -0,0 +1,9 @@
+INSERT INTO client (id, pseudo, email, nom, prenom, refCivilite, dateDeNaissance) VALUES (1, daddyboy, françois, royal, ref@email.fr, 1, 13/03/2014);
+INSERT INTO client (id, pseudo, email, nom, prenom, refCivilite, dateDeNaissance) VALUES (2, emma33, emma, briot, fred9d@emil.fr,2, 16/03/2014);
+INSERT INTO client (id, pseudo, email, nom, prenom, refCivilite, dateDeNaissance) VALUES (3, girlpower, gaelle, françois, ffd@emil.fr,3, 19/03/2014);
+INSERT INTO client (id, pseudo, email, nom, prenom, refCivilite, dateDeNaissance) VALUES (4, bodyboy, manuel, valls, nara@emil.fr,1, 17/03/2014);
+INSERT INTO client (id, pseudo, email, nom, prenom, refCivilite, dateDeNaissance) VALUES (5, fredystar, fred, gaff, vcd9@emil.fr,1, 13/03/2014);
+INSERT INTO client (id, pseudo, email, nom, prenom, refCivilite, dateDeNaissance) VALUES (6, francky, francky, hollande, vd97@emil.fr,1, 02/03/2014);
+INSERT INTO client (id, pseudo, email, nom, prenom, refCivilite, dateDeNaissance) VALUES (7, fv33, vincent, labriot, vcd44@emil.fr,1, 13/03/2014);
+INSERT INTO client (id, pseudo, email, nom, prenom, refCivilite, dateDeNaissance) VALUES (8, ma97, emmaelle, briot, vid@emil.fr,2, 13/03/2014);
+INSERT INTO client (id, pseudo, email, nom, prenom, refCivilite, dateDeNaissance) VALUES (9, em33, manuella, vincent, vdzz@emil.fr,3, 07/03/2014);
