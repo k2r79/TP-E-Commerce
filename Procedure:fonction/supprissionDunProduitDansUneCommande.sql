13bac31082edda84a53f1e6663c1d4fabdd94819
fredoprog <fredoprog@gmail.com>
2014-04-17 13:24:48 +0200

    procedure de suppression de d'un produit dans une commande

--- Procedure:fonction/supprissionDunProduitDansUneCommande.sql
+++ Procedure:fonction/supprissionDunProduitDansUneCommande.sql
@@ -1,10 +1,19 @@
 
 /* Suppression de produit d'une commande */
 
-CREATE OR REPLACE PROCEDURE supprimerUnProduitDuneCommande ( v_produit IN produit%ROWTYPE, v_commande commande%ROWTYPE)
-IS
-	
+CREATE OR REPLACE PROCEDURE supprimerUnProduitDuneCommande ( p_idproduit IN produit.id%TYPE, p_idcommande commande.id%TYPE)
+IS	
+	nomProduit produit.libelle%TYPE;
 BEGIN 
-	/  	
+	
+	SELECT libelle INTO nomProduit
+	FROM produit
+	WHERE produit.id = p_idproduit;
+	
+	DELETE FROM commandeproduit
+		   WHERE refproduit = p_idproduit
+		   AND refcommande = p_idcommande;
+	
+	DBMS_OUTPUT.PUT_LINE('ce produit'|| nomProduit ||'a été supprimer de la commande');
 
-END;
+END supprimerUnProduitDuneCommande;
